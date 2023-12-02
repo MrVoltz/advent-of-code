e6e99bdf6722f189249fd86541858703cb7c3d25
MrVoltz <uzivatelmp@gmail.com>
2023-12-02 09:10:05 +0100

    day02-1 & 2

---
+++ 2023-scala/day02/part_one.scala
@@ -0,0 +1,28 @@
+import scala.io.Source
+
+@main def partOne(inputPath: String): Unit = {
+  val input = Source.fromFile(inputPath)
+
+  val limits = Map(
+    "red" -> 12,
+    "green" -> 13,
+    "blue" -> 14
+  )
+
+  case class Game(id: Int, turns: List[Map[String, Int]])
+
+  val gamePattern = """^Game ([0-9]+): (.+)$""".r
+  val pickPattern = """^([0-9]+) (red|green|blue)$""".r
+
+  def parseGame(game: String): Option[Game] = game match {
+    case gamePattern(idStr, turnsStr) =>
+      Some(Game(idStr.toInt, turnsStr.split("; ").map(turnStr => turnStr.split(", ").map({
+        case pickPattern(num, color) => (color, num.toInt)
+      }).toMap).toList))
+    case _ => None
+  }
+
+  println(input.getLines().flatMap(parseGame).collect({
+    case Game(id, turns) if turns.forall(turn => turn.forall({ case (color, num) => limits(color) >= num })) => id
+  }).sum[Int].toString)
+}
---
+++ 2023-scala/day02/part_two.scala
@@ -0,0 +1,25 @@
+import scala.io.Source
+
+@main def partTwo(inputPath: String): Unit = {
+  val input = Source.fromFile(inputPath)
+
+  case class Game(id: Int, turns: List[Map[String, Int]])
+
+  val gamePattern = """^Game ([0-9]+): (.+)$""".r
+  val pickPattern = """^([0-9]+) (red|green|blue)$""".r
+
+  def parseGame(game: String): Option[Game] = game match {
+    case gamePattern(idStr, turnsStr) =>
+      Some(Game(idStr.toInt, turnsStr.split("; ").map(turnStr => turnStr.split(", ").map({
+        case pickPattern(num, color) => (color, num.toInt)
+      }).toMap).toList))
+    case _ => None
+  }
+
+  println(input.getLines().flatMap(parseGame).collect({
+    case Game(id, turns) =>
+      turns.flatten.foldLeft(Map.empty[String, Int])({ case (res, (color, num)) =>
+        res.updated(color, res.getOrElse(color, 0).max(num))
+      }).values.product
+  }).sum[Int].toString)
+}
