0372b02e706fb4e0d858c2098c6802def317f6f1
MrVoltz <uzivatelmp@gmail.com>
2023-12-09 09:11:37 +0100

    day09-1&2

---
+++ 2023-scala/day09/part_one.scala
@@ -0,0 +1,15 @@
+package day09
+
+import scala.io.Source
+
+@main def partOne(inputPath: String): Unit = {
+  val input = Source.fromFile(inputPath)
+
+  def extrapolate(seq: List[Long]): Long =
+    if(seq.forall(x => x == 0)) 0
+    else if(seq.size == 1) seq.last
+    else seq.last + extrapolate(seq.sliding(2).map({ case List(a, b) => b - a }).toList)
+
+  println(input.getLines.map(_.split(" ").map(_.toLong).toList).map(extrapolate).sum)
+
+}
