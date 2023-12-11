27bbb5cf10a10159584cb8cfca9e4d78af3677f1
MrVoltz <uzivatelmp@gmail.com>
2023-12-11 08:26:07 +0100

    day11-1&2

---
+++ 2023-scala/day11/part_two.scala
@@ -0,0 +1,41 @@
+package day11
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+import scala.io.Source
+
+@main def partOne(inputPath: String): Unit = {
+  val input = Source.fromFile(inputPath)
+
+  val lines = input.getLines().toIndexedSeq
+  val height = lines.size
+  val width = lines.head.length
+
+  case class Pos(y: Long, x: Long) extends Ordered[Pos] {
+    import scala.math.Ordered.orderingToOrdered
+
+    def dist(that: Pos): Long = (y - that.y).abs + (x - that.x).abs
+
+    def compare(that: Pos): Int = (y, x) compare (that.y, that.x)
+  }
+
+  def charAt(y: Int, x: Int) = lines(y).charAt(x)
+
+  val emptyRows = (for(y <- 0 until height if (0 until width).forall(x => charAt(y, x) == '.')) yield y).toSet
+  val emptyCols = (for(x <- 0 until width if (0 until height).forall(y => charAt(y, x) == '.')) yield x).toSet
+
+  @tailrec
+  def parseField(y: Int, x: Int, seenEmptyRows: Long, seenEmptyCols: Long, galaxies: Set[Pos]): Set[Pos] =
+    if(y >= height) galaxies
+    else if(x >= width) parseField(y + 1, 0, seenEmptyRows, 0, galaxies)
+    else if(emptyRows.contains(y)) parseField(y + 1, 0, seenEmptyRows + 1000000 - 1, 0, galaxies)
+    else if(emptyCols.contains(x)) parseField(y, x + 1, seenEmptyRows, seenEmptyCols + 1000000 - 1, galaxies)
+    else if(charAt(y, x) == '#') parseField(y, x + 1, seenEmptyRows, seenEmptyCols, galaxies + Pos(y + seenEmptyRows, x + seenEmptyCols))
+    else parseField(y, x + 1, seenEmptyRows, seenEmptyCols, galaxies)
+
+  val galaxies = parseField(0, 0, 0, 0, Set.empty)
+
+  println(galaxies)
+
+  println((for(from <- galaxies.iterator; to <- galaxies.iterator if to > from) yield from.dist(to)).sum)
+}
